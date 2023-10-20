49f71498e4a66e523aa67ad7d285ca95a4fcba05
EastOrb <102253515+EastOrb@users.noreply.github.com>
2023-10-21 00:33:13 +0100

    Update RewardToken.sol
    1. The unnecessary `_tokenIds.increment();` line was removed from the `awardItem` function. I noticed that was a duplicated line of code
    
    2. Comments have been added to explain the purpose of the `awardItem` and `redeemItem` functions for better documentation.

--- backend/contracts/RewardToken.sol
+++ backend/contracts/RewardToken.sol
@@ -11,21 +11,15 @@ contract RewardItem is ERC721URIStorage {
 
     constructor() ERC721("RewardItem", "TW") {}
 
-    function awardItem(
-        address player,
-        string memory _tokenURI
-    ) public returns (uint256) {
+    function awardItem(address player, string memory _tokenURI) public returns (uint256) {
         _tokenIds.increment();
-
         uint256 newItemId = _tokenIds.current();
         _mint(player, newItemId);
         _setTokenURI(newItemId, _tokenURI);
-
-        _tokenIds.increment();
         return newItemId;
     }
 
-    //redeem Item function that checks if the item exists , then burns it , and returns a bool
+    // Redeem an item and burn it
     function redeemItem(uint256 itemId) public returns (bool) {
         require(_exists(itemId), "Item does not exist");
         require(ownerOf(itemId) == msg.sender, "Item does not belong to you");
